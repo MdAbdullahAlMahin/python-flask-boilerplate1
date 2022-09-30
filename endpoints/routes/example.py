449a1e5e442ff397534ec0950f26f5dfdf27138f
MdAbdullahAlMahin <u3576752@connect.hku.hk>
2022-09-30 11:00:07 +0800

    pymongo set up

--- endpoints/routes/example.py
+++ endpoints/routes/example.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from pymongo import MongoClient
 
 from flask import request, jsonify
 
@@ -10,8 +11,8 @@ logger = logging.getLogger(__name__)
 
 @app.route('/example', methods=['GET'])
 def example():
-    output = {
-        "data": 50,
-    }
-
-    return jsonify(output)
+    client = pymongo.MongoClient("mongodb+srv://MdAbdullahAlMahin:<password>@cluster0.7nvntxs.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    db = client['testDB']
+    collection = db['testCol']
+    doc_count = collection.count_documents({})
+    return jsonify(doc_count)
